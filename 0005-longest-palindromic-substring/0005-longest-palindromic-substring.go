ce539027ab309cdf62747d374665577222576f65
Jirotore <98616889+Jirotore@users.noreply.github.com>
2026-06-16 13:47:00 +0700

    Time: 362 ms (9.95%), Space: 4.5 MB (31.54%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -1,12 +1,12 @@
 func longestPalindrome(s string) string {
-	srune := []rune(s)
-    if len(srune) < 2 {
+	// srune := []rune(s)
+    if len(s) < 2 {
         return s
     }
-	var longest []rune
-    for i := 0; i < len(srune); i++ {
-		for j := i; j < len(srune); j++ {
-            buf := srune[i:j+1]
+	var longest string
+    for i := 0; i < len(s); i++ {
+		for j := i; j < len(s); j++ {
+            buf := s[i:j+1]
 			if !check(buf) {
 				continue
 			}
@@ -18,7 +18,7 @@ func longestPalindrome(s string) string {
 	return string(longest)
 }
 
-func check(s []rune) bool {
+func check(s string) bool {
 	i, j := 0, len(s)-1
 	for i < j {
 		if s[i] != s[j] {
